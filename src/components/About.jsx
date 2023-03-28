d8fa07e317b889999c68086416bd0be13c772d95
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-28 15:00:38 +0100

    added styled components, created separate sections/containers for each page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  height: 100vh;
+  background-color: #5879ee;
+`
 
 const About = () => {
   return (
-    <div>About</div>
+    <Section>About</Section>
   )
 }
 
