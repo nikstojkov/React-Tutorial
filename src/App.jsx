26a9c61277b0d36ba885b665a0366c076e62a334
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-28 14:43:48 +0100

    created components and imported to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+
 function App() {
 
   return (
     <div>
-      Hello world!
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Contact/>
     </div>
   )
 }
